5e3b89aacd80b3f2420fadc133a4467bcf402850
hzlzh <hzlzh.dev@gmail.com>
2014-03-04 01:58:33 +0800

    update main files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,7 @@
-/*
- Grunt Workflow v1.3
-
- 一个完整的Grunt工作流，其中包含：
-    Less编译CSS
-    CSS压缩
-    自动合并雪碧图
-    自动处理Retina 2x适配
-    文件变动监控
-    FTP发布部署
-    Zip打包
-
- https://github.com/Mobile-Team/Grunt-Workflow
+/**
+ * Grunt Workflow v1.5
+ * https://github.com/Mobile-Team/Grunt-Workflow
+ * @hzlzh <hzlzh.dev@gmail.com>
  */
 
 // grunt-timer 返回各个task的执行时间
@@ -21,47 +12,49 @@ module.exports = function (grunt) {
     // grunt-timer 初始化
     timer.init(grunt);
 
+    // Grunt 配置初始化
     grunt.initConfig({
 
-        // 获取 package.json 依赖
+        // 读取 package.json 依赖
         pkg: grunt.file.readJSON('package.json'),
 
-        // Less 编译 CSS
+        // Less 编译 CSS，若使用 Sass 请自行替换
         less: {
             // 编译
             
-            // 分支 -> 开发向
+            // less 分支 -> 开发向
             dev: {
                 files: [
                     {
-                        expand: true, //启用动态扩展
-                        cwd: 'css/', // css文件源的文件夹
+                        expand: true, // 启用动态扩展
+                        cwd: 'css/', // CSS 文件源的文件夹
                         src: ['*.less', '!import*.less'], // 匹配规则
-                        dest: 'tmp/css/', //导出css和雪碧图的路径地址
-                        ext: '.css' // 导出的css名
+                        dest: 'tmp/css/', //导出 CSS 和雪碧图的路径地址
+                        ext: '.css' // 导出的 CSS 名
                     }
                 ],
                 options: {
                     yuicompress: false
                 }
             },
-            // 分支 -> 发布向
+            // less 分支 -> 发布向
             release: {
                 files: [
                     {
                         expand: true, //启用动态扩展
-                        cwd: 'css/', // css文件源的文件夹
+                        cwd: 'css/', // CSS 文件源的文件夹
                         src: ['*.less', '!import*.less'], // 匹配规则
-                        dest: 'tmp/css/', //导出css和雪碧图的路径地址
-                        ext: '.css' // 导出的css名
+                        dest: 'tmp/css/', //导出 CSS 和雪碧图的路径地址
+                        ext: '.css' // 导出的 CSS名
                     }
                 ],
                 options: {
-                    yuicompress: true
+                    yuicompress: true // 开启 YUI CSS 压缩 (http://yui.github.io/yuicompressor/)
                 }
             }
         },
-        // CSS 语法检查
+
+        // CSS 验证检查 (https://github.com/gruntjs/grunt-contrib-csslint)
         csslint: {
             options: {
                 formatters: [
@@ -82,7 +75,8 @@ module.exports = function (grunt) {
                 src: ['publish/css/*.css']
             }
         },
-        // 压缩css
+
+        // CSS 压缩 (https://github.com/gruntjs/grunt-contrib-cssmin)
         cssmin: {
             min: {
                 files: [
@@ -96,84 +90,94 @@ module.exports = function (grunt) {
                 ]
             }
         },
-        // 自动雪碧图
+
+        // 自动合并生成雪碧图
         sprite: {
-            // This is are multitask, you can create multiple sprite generators buy copying all
-            // object with other name, see grunt.js docs for details
             sprite: {
                 files: [
                     {
-                        expand: true, //启用动态扩展
-                        cwd: 'tmp/css', // css文件源的文件夹
+                        expand: true, // 启用动态扩展
+                        cwd: 'tmp/css', // CSS 文件源的文件夹
                         src: ['*.css'], // 匹配规则
-                        dest: 'tmp/', //导出css和雪碧图的路径地址
-                        ext: '.sprite.css' // 导出的css名
+                        dest: 'tmp/', // 导出 CSS 和雪碧图的路径地址
+                        ext: '.sprite.css' // 导出的 CSS 名
                     }
                 ],
                 // options
                 options: {
-                    // OPTIONAL: Rendering engine: auto, canvas, gm
+                    // 图片处理引擎选择，如：`GraphicsMagick` 或 `ImageMagick`
                     'engine': 'gm',
-                    // OPTIONAL: Image placing algorithm: top-down, left-right, diagonal, alt-diagonal
+                    // 图像合并排列算法: (默认使用最优二叉树法) top-down, left-right, diagonal, alt-diagonal
                     'algorithm': 'binary-tree'
-
                 }
             }
         },
+
+        // 复制文件夹操作
         copy: {
-            // 移动 slice/ 到 tmp/ 供下一步的 sprite Task 使用
+
+            // 移动 slice/ 到 tmp/ 供下一步的 合并雪碧图 task 使用
             slice: {
                 files: [
-                    {expand: true, cwd: 'slice/', src: ['**'], dest: 'tmp/slice/'}, // makes all src relative to cwd
+                    {expand: true, cwd: 'slice/', src: ['**'], dest: 'tmp/slice/'},
                 ]
             },
-            // 分支 -> 开发向
+
+            // copy 分支 -> 开发向
             dev: {
                 files: [
-                    {expand: true, cwd: 'tmp/css/', src: ['**', '!*.sprite.css', '!*.min.css'], dest: 'publish/css/'}, // makes all src relative to cwd
-                    {expand: true, cwd: 'slice/', src: ['**'], dest: 'publish/slice/'}, // makes all src relative to cwd
-                    {expand: true, cwd: 'img/', src: ['**'], dest: 'publish/img/'}, // makes all src relative to cwd
+                    {expand: true, cwd: 'tmp/css/', src: ['**', '!*.sprite.css', '!*.min.css'], dest: 'publish/css/'},
+                    {expand: true, cwd: 'slice/', src: ['**'], dest: 'publish/slice/'},
+                    {expand: true, cwd: 'img/', src: ['**'], dest: 'publish/img/'},
                 ]
             },
-            // 分支 -> 发布向
+
+            // copy 分支 -> 发布向
             release: {
                 files: [
-                    {expand: true, cwd: 'tmp/css/', src: ['*.css', '!*.sprite.css', '!*.min.css'], dest: 'release/css/'}, // makes all src relative to cwd
-                    {expand: true, cwd: 'img/', src: ['**'], dest: 'release/img/'}, // makes all src relative to cwd
-                    {expand: true, cwd: 'tmp/sprite/', src: ['**'], dest: 'release/sprite/'} // makes all src relative to cwd
+                    {expand: true, cwd: 'tmp/css/', src: ['*.css', '!*.sprite.css', '!*.min.css'], dest: 'release/css/'},
+                    {expand: true, cwd: 'img/', src: ['**'], dest: 'release/img/'},
+                    {expand: true, cwd: 'tmp/sprite/', src: ['**'], dest: 'release/sprite/'}
                 ]
             },
-            // 分支 -> 调试向
+
+            // copy 分支 -> 调试向
             debug: {
                 files: [
-                    {expand: true, cwd: 'tmp/css/', src: ['*.css', '!*.sprite.css', '!*.min.css'], dest: 'publish/css/'}, // makes all src relative to cwd
-                    {expand: true, cwd: 'img/', src: ['**'], dest: 'publish/img/'}, // makes all src relative to cwd
-                    {expand: true, cwd: 'tmp/sprite/', src: ['**'], dest: 'publish/sprite/'} // makes all src relative to cwd
+                    {expand: true, cwd: 'tmp/css/', src: ['*.css', '!*.sprite.css', '!*.min.css'], dest: 'publish/css/'},
+                    {expand: true, cwd: 'img/', src: ['**'], dest: 'publish/img/'},
+                    {expand: true, cwd: 'tmp/sprite/', src: ['**'], dest: 'publish/sprite/'}
                 ]
             }
         },
+
+        // 检测 文件/代码 变动事件
         watch: {
             files: 'css/*.less',
             tasks: ['less:dev', 'copy:dev', 'clean:dev']
         },
+
+        // FTP 部署，上传 release/ 所有文件到预先设置的FTP
         'ftp-deploy': {
             push: {
                 auth: {
-                    host: '119.147.200.113',
+                    host: 'xxx.xxx.xxx.xxx',
                     port: 21000,
-                    authKey: 'lifestyle'
+                    authKey: 'xxx'
                 },
                 src: 'release/',
                 dest: 'proj-<%= pkg.name %>/',
-                exclusions: ['**/.DS_Store', '**/Thumbs.db', 'tmp']
+                exclusions: ['**/.DS_Store', '**/Thumbs.db', 'tmp'] // 不上传文件类型
             }
         },
+
+        // PNG 压缩 (更多配置说明：https://www.npmjs.org/package/grunt-pngmin)
         pngmin: {
             compile: {
                 options: {
-                    ext: '.png',
-                    force: true,
-                    iebug: true //IE6
+                    ext: '.png',  // 后缀名
+                    force: true,  // 生成优化后的图片覆盖原始图片
+                    iebug: false  // 为 IE6 优化图片，如需要可设置`true`
                 },
                 files: [
                     {
@@ -190,33 +194,42 @@ module.exports = function (grunt) {
                 ]
             }
         },
+
+        // 自动生成 @2x 图片对应的 @1x 图 (已存在图片不再生成，仅缺失图片触发此操作) 
         _2x2x: {
             scale: {
-                imgsrcdir: "slice",
-                imgdesdir: "slice",
+                imgsrcdir: "slice", // 源目录，此目录中的 @2x -> @1x
+                imgdesdir: "slice", // 目标目录
                 option: {
-                    'overwrite': true
+                    'overwrite': true // 是否覆盖原图
                 }
             }
         },
+
+        // 清理临时目录
         clean: {
+            // clean 开发向
             dev: ['tmp/','publish/sprite/', 'release/'],
+            // clean 发布向
             release: ['tmp/', 'publish/', 'release/'],
+            // clean 调试向
             debug: ['tmp/', 'publish/slice/']
         },
+
+        // 文件夹打包压缩 Zip
         compress: {
             main: {
                 options: {
-                    archive: 'proj-<%= pkg.name %>-' + 'release.zip'
+                    archive: 'proj-<%= pkg.name %>-' + 'release.zip' // 设置压缩包名称
                 },
                 files: [
-                    { expand: true, src: "**/*", cwd: "release/" }
+                    { expand: true, src: "**/*", cwd: "release/" } // 设置压缩范围为整个 `release/` 发布目录
                 ]
             }
         }
     });
 
-    // Offical
+    // 加载官方插件
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-csslint');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
@@ -225,27 +238,44 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-compress');
 
-    // Plugins
+    // 加载其他插件
     grunt.loadNpmTasks('grunt-sprite');
     grunt.loadNpmTasks('grunt-ftp-deploy');
     grunt.loadNpmTasks('grunt-pngmin');
+    grunt.loadNpmTasks('grunt-2x2x');
 
-    // Basic Tasks
+    
 
-    // 默认流，输出 publish['css', 'img', 'slice']
-    // 仅Less->CSS，无其他操作
+    /* 任务注册开始 */
+
+    // == 默认工作流 ==
+    // 输出目录为：../publish/(css/ + img/ + slice/)
+    // 注：仅做编译操作 Less/Sass -> CSS，无其他操作
     grunt.registerTask('default', ['less:dev', 'copy:dev', 'clean:dev', 'watch']);
 
-    // 完全发布流，输出 release['css', 'img', 'sprite']
+    // == 完整发布流 ==
+    // 输出目录为：../publish/(css/ + img/ + sprite/)
+    // 注：包括 Less/Sass 编译+压缩+雪碧图拼合+PNG压缩，仅执行1次流，不含(文件变动 watch)
     grunt.registerTask('all', ['less:release', 'sprite-cssmin', 'copy:release']);
 
-    // 调试流，输出 publish['css', 'img', 'sprite']，不清除 [tmp]
+    // == 调试工作流 ==
+    // 输出目录为：../publish/(css/ + img/ + sprite/)
+    // 注：同 `grunt all`，但不删除 tmp/ 目录，供调试查看使用，含(文件变动 watch)
     grunt.registerTask('debug', ['clean:release', 'less:release', 'sprite-cssmin', 'copy:debug', 'watch']);
 
-    // 两种刚发布方向 ftp上传 和 本地打包
+    // == FTP 发布操作 ==
+    // 注：将 `grunt all` 生成结果使用 FTP 上传到服务端
     grunt.registerTask('push', ['all', 'ftp-deploy:push', 'clean:release']);
+
+    // == ZIP 发布操作 ==
+    // 注：将 `grunt all` 生成结果使用 ZIP 生成包文件
     grunt.registerTask('zip', ['all', 'compress', 'clean:release']);
 
-    // 别名：移动slice -> 合并sprite -> css压缩
+    // 定义别名 `grunt sprite-cssmin`
+    // 注：拷贝移动 slice -> 合并雪碧图 sprite -> CSS 压缩
     grunt.registerTask('sprite-cssmin', ['copy:slice', 'sprite', 'cssmin']);
+
+    // 定义别名 `grunt 2x2x`
+    // 注：@2x 图 生成 @1x 图
+    grunt.registerTask('2x2x', ['_2x2x']);
 }
--- README.md
+++ README.md
@@ -1,40 +1,141 @@
-# Grunt Workflow v1.3
+# Grunt Workflow v1.3 [![Bower version](https://badge.fury.io/bo/grunt-workflow.png)](http://badge.fury.io/bo/grunt-workflow) [![Build Status](https://secure.travis-ci.org/hzlzh/Grunt-Workflow.png)](http://travis-ci.org/hzlzh/Grunt-Workflow) [![devDependency Status](https://david-dm.org/hzlzh/Grunt-Workflow/dev-status.png?theme=shields.io)](https://david-dm.org/hzlzh/Grunt-Workflow#info=devDependencies)
 
-## 一个完整的Grunt工作流，其中包含Task：
+
+**Grunt Workflow** 是一个基于 [Grunt]的 跨平台（Mac&Win）、优雅的、高效的、可定制的前端重构工作流程。
+
+### 快速开始
+
+提供以下三种方式获取：
+
+- [下载最新版 Grunt Workflow]
+- Clone the repo: `git clone 
+- 用 [Bower](http://bower.io) 安装: `bower install grunt-workflow`.
+
+阅读 [说明文档] 了解更多.
+
+### 文件结构
+
+安装完成后，你将得到以下文件结构：
+
+```
+Grunt-Workflow/
+│
+├── package.json                // 项目依赖定义
+├── Gruntfile.js                // 配置任务
+├── .ftppass                    // FTP 部署密码(非必选)
+│
+├── node_modules    // FTP 部署密码(非必选)
+│
+├── html/                   // HTML文件
+│   └── index.html
+├── css/                        // CSS源文件(通常为`Less`/`Sass`等)
+│   ├── lib-reset.less
+│   ├── lib-mixins.less
+│   └── style.less
+├── img/                        // 图片素材 [*非*合并 Sprite(雪碧图)] 如：logo
+│   ├── logo.png
+│   └── background.png
+├── slice/                      // 图片素材 [待自动合并 Sprite] 如：Icons
+│   ├── icon-github.png
+│   ├── icon-github@2x.png      // 含 1x & 2x 图
+│   ├── icon-apple.png
+│   ├── icon-apple@2x.png
+└── publish/                    // 目标文件夹，存放可发布的成品
+    ├── css/                    // 最终 CSS 成品
+    │   └── style.css
+    ├── img/                    // 仅 Copy 不错操作
+    │   ├── logo.png
+    │   └── background.png
+    └── sprite/                 // 自动生成的雪碧图
+        └── demo.png
+```
+经过 Grunt Workflow 的自动化过程，`../publish`目录下为最终输出
+
+### 环境准备
+
+#### Mac OS
+
+1. 建议使用 Brew 安装 Node.js [默认此步骤已完成]
+2. 单独安装下面两个依赖 [注](https://github.com/Ensighten/spritesmith#requirements)
+
+        // 安装 GraphicsMagick 图像处理库
+        brew install GraphicsMagick
+        
+        // 安装 Phantomjs 库
+        brew install phantomjs
+
+3. 运行 `gm version` 和 `phantomjs --version` 来测试上述依赖是否成功安装完成
+
+#### Windows
+
+1. 安装 [Node.js] *默认此步骤已完成*
+2. 下载安装 [GraphicsMagick] 和 [Phantomjs]  
+如：[GraphicsMagick-1.3.19-Q16-win32-dll.zip](https://raw.github.com/hzlzh/Grunt-Workflow/assets/download/GraphicsMagick-1.3.19-Q16-win32-dll.zip)
+如：[phantomjs-1.9.2-windows.zip](https://raw.github.com/hzlzh/Grunt-Workflow/assets/download/phantomjs-1.9.2-windows.zip)
+3. 运行安装完成后，[添加环境变量](https://www.evernote.com/shard/s50/sh/18f4d9ad-b55d-46e5-8fc4-a45452ccfdc9/0f4b9b7fe459888337f48689ced99068)
+
+### 说明文档
+
+一个完整的 `Grunt 工作流`，其中包含 `Task`：
     
-* Less编译CSS
-* CSSLint检验
-* CSS压缩
+* Less/Sass 编译为 CSS
+* CSS Lint 检验
+* CSS 压缩
 * 自动合并雪碧图
-* 自动处理Retina 2x适配
-* 文件变动监控
-* FTP发布部署
-* Zip打包
+* 自动处理 Retina 2x 适配
+* 自动追加 CSS 文件末时间戳
+* 文件变动检测，触发编译
+* FTP 发布部署
+* ZIP 打包项目
 
-## 命令包括：
+## 命令详解：
 
-#### `grunt`  
+#### 默认工作流 `grunt`
 
-默认流，输出 publish['css', 'img', 'slice']  
-仅Less->CSS，无其他操作  
+`grunt.registerTask('default', ['less:dev', 'copy:dev', 'clean:dev', 'watch']);`
 
-#### `grunt all`
-完全发布流，输出 release['css', 'img', 'sprite']
+输出目录为：../publish/(css/ + img/ + slice/)  
+注：仅做编译操作 Less/Sass -> CSS，无其他操作  
 
-#### `grunt debug`
-调试流，输出 publish['css', 'img', 'sprite']，不清除 [tmp]
 
-#### `grunt push`
-将release版本，FTP上传
+#### 完整发布流 `grunt all`
 
-#### `grunt zip`
-将release版本，Zip打包到根目录，命名为`proj-xxx-release.zip`
+输出目录为：`../publish/(css/ + img/ + sprite/)`  
+注：包括 Less/Sass 编译+压缩+雪碧图拼合+PNG压缩，仅执行1次流，不含(文件变动 watch)
 
-## 已解决问题
+#### 调试工作流 `grunt debug`
 
-* Mac & Win下的版本下 CPU图片压缩算法不同导致的 版本库Diff问题
-* 去除了开发过程中频繁的编译、压缩、合并图片等操作
+输出目录为：`../publish/(css/ + img/ + sprite/)`  
+注：同 `grunt all`，但不删除 `tmp/` 目录，供调试查看使用，含(文件变动 watch)
+
+#### FTP 发布操作 `grunt push`
+
+注：将 `grunt all` 生成结果使用 FTP 上传到服务端
+
+#### ZIP 发布操作 `grunt zip`
+
+注：将 `grunt all` 生成结果使用 ZIP 生成包文件  
+
+#### 定义别名 `grunt sprite-cssmin`
+
+注：拷贝移动 `slice/` -> 合并雪碧图 `sprite` -> CSS 压缩
+
+#### 定义别名 `grunt 2x2x`
+
+注：@2x 图 生成 @1x 图
+
+
+## 已知特性
+
+* 由于 `Mac OS` & `Win` 底层 `CPU图片压缩算法` 不同导致的版本库`Diff`问题
+* 移除了开发过程中频繁的编译、压缩、合并图片等操作，开发全程使用 `@1x` 图预览
 
 ## 示例图
 
-![](http://ww3.sinaimg.cn/large/644eac00gw1e9woj8gddmj20cr0mijuj.jpg)
+(http://ww3.sinaimg.cn/large/644eac00gw1e9woj8gddmj20cr0mijuj.jpg)
+
+
+[说明文档]: 
+[Node.js]: http://nodejs.org/
+[GraphicsMagick]: http://www.graphicsmagick.org/
+[Phantomjs]: http://phantomjs.org/
